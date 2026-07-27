c8095359fca7c5a8ed8ca42ff8cd791234be477b
BigMacWack <bigmacwack@mail.com>
2026-07-26 21:34:31 -0400

    Load lineage-first tracker styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import "./compact.css";
 import "./planner-fix.css";
+import "./tracker-inspired.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
